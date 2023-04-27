7af0d97b5b50f9af876731ace042116ab8fb9212
Ted - TedfordMedia <ted@tedfordmedia.com>
2023-04-27 02:01:02 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,30 +2,31 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 const path = require("path");
+
 module.exports = {
   siteMetadata: {
     title: `test-gatsby`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.tedfordmedia.co.uk`
   },
-
-  plugins: ["gatsby-plugin-sitemap", "gatsby-plugin-mdx", {
-    resolve: 'gatsby-plugin-alias-imports',
-    options: {
-      alias: {
-        "@components": path.resolve(__dirname, "src/components"),
-        // "@helpers": path.resolve(__dirname, "src/helpers"),
-        // "@images": path.resolve(__dirname, "static/images"),
-        // "@models": path.resolve(__dirname, "static/models"),
-        // "@styles": path.resolve(__dirname, "src/styles"),
-        // "react-dom": "@hot-loader/react-dom",
+  plugins: [
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-mdx",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "pages",
+        "path": "./src/pages/"
       },
-      extensions: []
+      __key: "pages"
     },
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+    {
+      resolve: 'gatsby-plugin-alias-imports',
+      options: {
+        alias: {
+          "@components": path.resolve(__dirname, "src/components"),
+        },
+        extensions: []
+      }
+    }
+  ]
 };
