262136912fe1e8fca78754ace4a80c2b85c077fb
Ted - TedfordMedia <ted@tedfordmedia.com>
2023-04-27 04:08:53 +0100

    update

--- src/components/BrainGltf.jsx
+++ src/components/BrainGltf.jsx
@@ -8,7 +8,7 @@ import gsap from "gsap";
 import { BrainMarker } from './BrainMarker';
 
 export function Brain(props) {
-  const { nodes, materials } = useGLTF('static/epilep3.glb')
+  const { nodes, materials } = useGLTF('ss.glb')
   const boneOpacity = useSceneStore((state) => state.boneOpacity);
   const skinOpacty = useSceneStore((state) => state.skinOpacty);
   const boneDecal = useSceneStore((state) => state.boneDecal);
@@ -146,4 +146,4 @@ export function Brain(props) {
   )
 }
 
-useGLTF.preload('static/epilep3.glb')
+useGLTF.preload('ss.glb')
